050fed5043747d1930f316c28db6334638d8348d
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2022-02-07 08:52:12 -0800

    Initial commit

---
+++ src/main/java/website/skylorbeck/miniminer/Miniminer.java
@@ -0,0 +1,10 @@
+package website.skylorbeck.miniminer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Miniminer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/website/skylorbeck/miniminer/client/MiniminerClient.java
@@ -0,0 +1,13 @@
+package website.skylorbeck.miniminer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MiniminerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
